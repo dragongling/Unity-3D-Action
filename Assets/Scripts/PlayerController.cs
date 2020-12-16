52f5d0f76871a1638e280d754554efd929d4cb7b
Alexander Slesarenko <dragongling@gmail.com>
2020-12-16 11:32:23 +0500

    Jump implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,9 +11,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField, Range(0f, 100f)]
     float maxAcceleration = 10f;
 
-    Vector3 velocity;
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+
+    Vector3 velocity, desiredVelocity;
 
     Rigidbody body;
+
+    bool desiredJump;
+    
 
     private void Awake()
     {
@@ -24,16 +31,34 @@ public class PlayerController : MonoBehaviour
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
+        desiredJump |= Input.GetButtonDown("Jump");            
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
         
-        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;        
+    }
+
+    private void FixedUpdate()
+    {
         velocity = body.velocity;
+
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }        
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x =
             Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z =
             Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
         body.velocity = velocity;
+    }
+
+    private void Jump()
+    {
+        Debug.Log("Jump desired!");
+        velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
     }
 }
